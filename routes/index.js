9a91972322a485d639b55dc4107aefaba47ad0bd
Garrag <347891760@qq.com>
2017-09-25 23:40:57 +0800

    首页改版

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,14 @@
 var express = require('express');
 var router = express.Router();
-// var mysql = require('./dao/mysql.js');
 
 router.all('/', function(req, res, next) {
     var data = {};
     res.render('index', data);
 });
 
-/**
- * 获取新闻内容
- */
-router.get('/news', function(req, res, next) {
+router.all('/product_info', function(req, res, next) {
     var data = {};
-    res.render('item/news', data);
+    res.render('product_info', data);
 });
 
 
