9b29cc6723bdba20555df441ecc0001ea3258e20
Garrag <347891760@qq.com>
2017-09-27 01:04:29 +0800

    基本成形

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var path = require("path")
+var fs = require('fs');
+
+
+var gongyePath = path.join(__dirname, "../excel/product_gongye.json");
+var huangbaoPath = path.join(__dirname, "../excel/product_huangbao.json");
+var shiyongPath = path.join(__dirname, "../excel/product_shiyong.json");
+var yiqiPath = path.join(__dirname, "../excel/product_yiqi.json");
+var tuliaoPath = path.join(__dirname, "../excel/product_tuliao.json");
+
+
+var dataMap = {
+    1 : JSON.parse(fs.readFileSync(gongyePath)),
+    2 : JSON.parse(fs.readFileSync(huangbaoPath)),
+    3 : JSON.parse(fs.readFileSync(shiyongPath)),
+    4 : JSON.parse(fs.readFileSync(yiqiPath)),
+    5 : JSON.parse(fs.readFileSync(tuliaoPath))
+}
+
+for (var key in dataMap) {
+    for(var i=0; i<dataMap[key].length; i++){
+        dataMap[key][i].type = key
+        dataMap[key][i].id = i
+    }
+}
 
 router.all('/', function(req, res, next) {
-    var data = {};
-    res.render('index', data);
+    var type = req.query.index || 1;
+    var json = dataMap[type];
+
+    var newData = []
+    for (var i=0; i<json.length; i++) {
+        var element = json[i];
+        if(element.name && element.name != '') {
+            newData.push(element)
+        }
+    }
+    res.render('index', {listData : newData});
 });
 
 router.all('/product_info', function(req, res, next) {
-    var data = {};
+    var id = req.query.id || 0;
+    var type = req.query.type || 0;
+
+    var infoData = dataMap[type][id];
+    var data = {infoData : infoData};
     res.render('product_info', data);
 });
-
 
 module.exports = router;
--- xlsToJson.js
+++ xlsToJson.js
@@ -38,3 +38,27 @@ node_xj({
         console.log("食用化工原料,转化完成!!!");
     }
 });
+
+node_xj({
+    input: root + "data.xlsx", // input xls 
+    sheet: "化学试剂仪器", // specific sheetname 
+    output: outUrl + "product_yiqi.json" // output json 
+}, function(err, result) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("化学试剂仪器,转化完成!!!");
+    }
+});
+
+node_xj({
+    input: root + "data.xlsx", // input xls 
+    sheet: "艺术涂料", // specific sheetname 
+    output: outUrl + "product_tuliao.json" // output json 
+}, function(err, result) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("艺术涂料,转化完成!!!");
+    }
+});
